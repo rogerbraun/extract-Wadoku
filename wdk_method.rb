c2c1f3ac516ebbe4ef2850496fbb72feb4d4f230
Roger Braun <davinelulinvega@gmail.com>
2011-09-20 21:47:28 +0200

    modulize, more functional, better tests

--- wdk_method.rb
+++ wdk_method.rb
@@ -1,51 +1,49 @@
 #encoding: utf-8
 #myinsert dupliziert den Array
-class Array
-	def myinsert(pos, el)
-		self.dup.insert(pos, el)
-	end
-end
 
 module Wadoku
+  HW_regex = /(<HW.+?: )|(>)(?!\)|“)/
+  Filter_regex = /(\[.+?\]|<Prior.+?>|<JLPT.+?>|<GENKI.+?>|<LangNiv.+?>|<Usage.+?>|<JWD.+?>|<Jap.+?>|<DaID.+?>|<Etym.+?>|\(<Ref.+?>\))/
+  Tre_regex = /(?:<TrE:){1,}(.*?)(?=>>>|>;|>\. |>\ \/)/
 
-#seperiert Teil vom Eintrag mit dem gegebenen Index nach dem gegebenen Regex
-def sep(entry, index, regex)
-	entry.split("\t")[index].scan(regex).flatten
+  class Entry
+    attr_accessor :da_id, :schreibung, :lesung, :deutsch
+    def initialize(str)
+      arr = str.split("\t")
+      @da_id, @schreibung, @lesung, @deutsch = arr[0], arr[1],arr[3],arr[4]
+      @schreibung = @schreibung.split(/[;]/).map(&:strip)
+      @lesung = @lesung.split(/[;]/).map(&:strip)
+    end
+
+    def sub_entries
+      @schreibung.product(@lesung).map{|schreibung, lesung| "#{@da_id}\t#{schreibung}\t#{lesung}"}.product(tres).map{|first_part, tre| "#{first_part}\t#{tre}"}
+    end
+
+    def tres
+      @deutsch.gsub!(Filter_regex)
+    
+      tres = @deutsch.scan(Tre_regex).flatten
+      # cleanup
+      
+      tres.map do |tre|
+        tre.gsub(HW_regex,"").strip
+      end
+    end
+  end
+
+  #Datei extrahieren und in eine neue Datei schreiben
+  def self.extract(wdk_raw, wdk_new)
+    content = File.readlines(wdk_raw).drop(1)
+    new_content = content.map{|entry| Wadoku::Entry.new(entry).sub_entries}.flatten.join("\n")
+    file = File.new(wdk_new, "w")
+    file.puts new_content
+    file.close
+  end
+
 end
 
-#filtert die Index raus
-def filter_index entry
-	filter_regex = /(\[.+?\]|<Prior.+?>|<JLPT.+?>|<GENKI.+?>|<LangNiv.+?>|<Usage.+?>|<JWD.+?>|<Jap.+?>|<DaID.+?>|<Etym.+?>|\(<Ref.+?>\))/
- 	entry.gsub(filter_regex, "")
-end
-
-#generiert einen Eintrag
-def generate entry
-	filtered_entry = filter_index entry
-	e_array = filtered_entry.split("\t")
-	kana = e_array[3] ? 3 : 2
-	rest = e_array.values_at(0,kana)
-
-	writings = sep(filtered_entry , 1, /[^;\(\)]+/)
-	tres = sep(filtered_entry, 4, /(?:<TrE:){1,}(.*?)(?=>>>|>;|>\. |>\ \/)/)
-#.map{|tre| tre.end_with?(">") ? tre : tre + ">"}
-
-	acc = []
-	writings.each do |w|
-		tres.each{|tre| acc = acc << rest.myinsert(1, w).myinsert(4, tre).join("\t").gsub("\t\t", "\t")}
-	end
-	return acc
-end
-
-
-#Datei extrahieren und in eine neue Datei schreiben
-def extract(wdk_raw, wdk_new)
-	content = File.readlines(wdk_raw).drop(1)
-	new_content = content.map{|entry| generate entry}.flatten.join("\n")
-	file = File.new(wdk_new, "w")
-	file.puts new_content
-	file.close
-end
-
-HW_regex = /(<HW.+?: )|(>)(?!\)|“)/
+if ARGV.length < 2 then
+  puts "Usage: wdk_method.rb input_file output_file"
+else
+  Wadoku.extract(ARGV[0],ARGV[1])
 end
--- wdk_spec.rb
+++ wdk_spec.rb
@@ -1,8 +1,9 @@
 #encoding: utf-8
 require "./wdk_method.rb"
 
+describe Wadoku do
 
-  describe Wadoku do
+  describe "regexes" do 
     it "should parse <HW NAr: C>" do
     "<HW NAr: C>".gsub(Wadoku::HW_regex, "").should == "C"
     end
@@ -10,3 +11,41 @@ require "./wdk_method.rb"
     "<HW m: Vokal> „<Topic: a>“".gsub(Wadoku::HW_regex, "").should == "Vokal „<Topic: a>“"
     end
   end
+
+  describe Wadoku::Entry do
+    before(:each) do
+      @entry = "6376215\t一視同仁\t\tいっしどうじん\t(<POS: N.>) <MGr: <TrE: <HW f: Bruderschaft> aller Menschen>; <TrE: <HW f: Gleichheit> ohne Diskriminierung>; <TrE: universale <HW f: Menschenliebe>>>."
+
+      @gokuboso_str = "8720235	極細; ごく細	極細	ごくぼそ	(<POS: N.>) <MGr: <TrE: <HW f: Feinheit>>>.	名	0		HE					ごく[WaSep]ぼそ	"
+
+      @gokuboso_entry = Wadoku::Entry.new(@gokuboso_str)
+
+      @wdk_entry = Wadoku::Entry.new(@entry)
+
+    end
+
+    it "should initialize to a valid entry" do
+      @wdk_entry.schreibung.should == ["一視同仁"]
+      @wdk_entry.lesung.should == ["いっしどうじん"]
+      @wdk_entry.deutsch.should == "(<POS: N.>) <MGr: <TrE: <HW f: Bruderschaft> aller Menschen>; <TrE: <HW f: Gleichheit> ohne Diskriminierung>; <TrE: universale <HW f: Menschenliebe>>>."
+
+    end
+
+    describe "#sub_entries" do
+      it "should split entries into an Array of subentries" do
+        @wdk_entry.sub_entries.should == ["6376215\t一視同仁\tいっしどうじん\tBruderschaft aller Menschen", "6376215\t一視同仁\tいっしどうじん\tGleichheit ohne Diskriminierung", "6376215\t一視同仁\tいっしどうじん\tuniversale Menschenliebe"] 
+
+        @gokuboso_entry.sub_entries.should == ["8720235\t極細\tごくぼそ\tFeinheit","8720235\tごく細\tごくぼそ\tFeinheit"]
+      end
+    end
+
+    describe "#tres" do
+      it "should give you all translation equivalents, with all markup removed" do
+        @wdk_entry.tres.should == ["Bruderschaft aller Menschen", "Gleichheit ohne Diskriminierung", "universale Menschenliebe"]
+        @gokuboso_entry.tres.should == ["Feinheit"]
+
+      end
+    end
+  end
+
+end
